279cfa7fc84c0e2819047cbc2c907ce5e6450832
Beatriz Aquino <beatrizzoldyck07@gmail.com>
2023-05-24 18:17:00 -0400

    subindo

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
-const amount = 12 
+// const amount = 12 
 
-if(amount < 10) {
-    console.log("Small number")
-} else {
-    console.log("Large number")
+// if(amount < 10) {
+//     console.log("Small number")
+// } else {
+//     console.log("Large number")
+// }
+
+// console.log(`Hello, this is my code!`)  
+
+// setInterval(() => {
+//     console.log('Hello world')
+// }, 1000)
+
+const secret = 'SUPER SECRET'
+const john = 'john'
+const peter = 'peter'
+
+const sayHi = (name) => {
+    console.log(`Hello there ${name}`)
 }
 
-console.log(`Hello, this is my code!`)  
+sayHi('susan')
+sayHi(john)
+sayHi(peter)
+
